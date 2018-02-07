cf81818bfe1fe5c1611f45e8c37b792751fbabae
zirr <31447312+zirriously@users.noreply.github.com>
2018-02-07 08:59:24 +0100

    Loading of paths now works.

--- PraiseTheSync/Program.cs
+++ PraiseTheSync/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace PraiseTheSync
 {
@@ -10,6 +13,34 @@ namespace PraiseTheSync
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Fetching saves...");
+
+            // Load paths from json
+            List<string> paths = new List<string>();
+            string backupLoc = String.Empty;
+
+            string[] lines = File.ReadAllLines(@"C:\Repos\Projects\PraiseTheSync\PraiseTheSync\Paths.txt");
+
+            foreach (var value in lines)
+            {
+                if (value.StartsWith("+"))
+                {
+                    string temp = value.Remove(0, 1);
+                    paths.Add(temp);
+                }
+                else if (value.StartsWith("-"))
+                {
+                    backupLoc = value.Remove(0, 1);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid path | " + value);
+                }
+            }
+
+            var saveFiles = new SaveFiles(paths, backupLoc);
+            Console.ReadKey();
+
         }
     }
 }
--- PraiseTheSync/SaveFiles.cs
+++ PraiseTheSync/SaveFiles.cs
@@ -8,5 +8,13 @@ namespace PraiseTheSync
 {
     class SaveFiles
     {
+        private List<string> _paths;
+        private string _backupLoc = String.Empty;
+
+        public SaveFiles(List<string> paths, string backupLoc)
+        {
+            _paths = paths;
+            _backupLoc = backupLoc;
+        }
     }
 }
