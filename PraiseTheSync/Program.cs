7b41f4b169d381663b6c4bc0993c4ab4b42f0050
zirr <31447312+zirriously@users.noreply.github.com>
2018-02-09 10:57:24 +0100

    Creates empty file if paths.txt does not exist.

--- PraiseTheSync/Program.cs
+++ PraiseTheSync/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,14 +15,29 @@ namespace PraiseTheSync
         static void Main(string[] args)
         {
             Console.WriteLine("Fetching saves...");
-
             // Load paths from ..\paths.txt
             List<string> paths = new List<string>();
             string backupLoc = String.Empty;
 
-            string[] lines = File.ReadAllLines(@"C:\Repos\Projects\PraiseTheSync\PraiseTheSync\Paths.txt");
+            string _emptyPathsTxt = "﻿/BACKUP FOLDER LOCATION BELOW THIS LINE" +
+                                    "\r\n/Only one for now, but will maybe make it work with multiple output locations later" +
+                                    "\r\n/Do not delete the \'.\' prefix.\r\n.Dummy\\Path\r\n/FOLDERS TO BACKUP\r\n/Do not delete the \'+\' prefix." +
+                                    "\r\n+Dummy\\Path\\One\r\n+Dummy\\Path\\Two\r\n+Dummy\\Path\\Three";
 
-            foreach (var value in lines)
+            if (File.Exists("Paths.txt"))
+            {
+                paths = File.ReadAllLines(@"Paths.txt").ToList();
+            }
+            else
+            {
+                Console.WriteLine("Paths.txt does not exist. It must be placed in the same directory as the executable." +
+                                  "Creating empty .txt file then exiting.");
+                System.IO.File.WriteAllText(_emptyPathsTxt, "Paths.txt");
+                System.Threading.Thread.Sleep(2000);
+                Environment.Exit(0);
+            }
+
+            foreach (var value in paths)
             {
                 if (value.StartsWith("+"))
                 {
